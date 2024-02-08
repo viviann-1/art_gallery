183005cf029f900c1a53b1d259945f0bdbecaf08
VIVIAN_N <VIVIAN_N@HOTMAIL.CO.UK>
2024-02-08 15:34:45 +0000

    created customer class

---
+++ src/main/java/Customer.java
@@ -0,0 +1,10 @@
+public class Customer {
+
+    private String name;
+    private double wallet;
+
+    public Customer(String inputName, double inputWallet){
+        this.name = inputName;
+        this.wallet = inputWallet;
+    }
+}
