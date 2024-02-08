7cbe622407282bf640541a3825de5d534ae3b9d7
VIVIAN_N <VIVIAN_N@HOTMAIL.CO.UK>
2024-02-08 16:07:56 +0000

    tested if gallery has a name

--- src/test/java/GalleryTest.java
+++ src/test/java/GalleryTest.java
@@ -1,19 +1,27 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class GalleryTest {
 
     Gallery gallery;
+    Artwork art1;
+    Artwork art2;
+    Artwork art3;
 
     @BeforeEach
     public void setUp() {
-//        Artwork art1 = new Artwork("Starry Night", 100, "Van Gogh");
-//        Artwork art2 = new Artwork("Dove", 200, "Picasso");
-//        Artwork art3 = new Artwork("Sunflower", 300, "Aebel");
-//
-//
-//
-//        gallery = new Gallery("Best Gallery Ever", 0, stock);
+        gallery = new Gallery("Best Gallery Ever");
+
+        art1 = new Artwork("Starry Night", 100, "Van Gogh");
+        art2 = new Artwork("Dove", 200, "Picasso");
+        art3 = new Artwork("Sunflower", 300, "Aebel");
+    }
+
+    @Test
+    public void gallaryHasName() {
+        assertThat(gallery.getName()).isEqualTo("Best Gallery Ever");
     }
 }
