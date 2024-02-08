2866a11a2024d06eb27f1fc7653cb37ee977e87f
VIVIAN_N <VIVIAN_N@HOTMAIL.CO.UK>
2024-02-08 15:29:32 +0000

    created artwork class

---
+++ src/main/java/Artwork.java
@@ -0,0 +1,11 @@
+public class Artwork {
+    private String title;
+    private double price;
+    private String artist;
+
+    public Artwork(String inputTitle, double inputPrice, String inputArtist){
+        this.title = inputTitle;
+        this.price = inputPrice;
+        this.artist = inputArtist;
+    }
+}
